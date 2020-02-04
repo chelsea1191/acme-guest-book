1d94c2ad7b6d9b24020c162f6d1b77771565b89c
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-02-03 19:42:28 -0500

    initial

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-console.log('hi chelsea');
+console.log("hi");
+const fs = require("fs");
+
+const readFile = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString());
+      }
+    });
+  });
+};
+
+const writeFile = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const addGuest = guest => {
+  return readFile("./guests.json")
+    .then(data => {
+      const guests = JSON.parse(data);
+      let max = guests.reduce((acc, guest) => {
+        if (guest.id > acc) {
+          acc = guest.id;
+        }
+        return acc;
+      }, 0);
+      guest.id = max + 1;
+      guests.push(guest);
+      return writeFile("./guests.json", JSON.stringify(guests, null, 2));
+    })
+    .then(() => {
+      return guest;
+    });
+};
